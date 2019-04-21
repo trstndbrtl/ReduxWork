54c4ad360744140bfbb69857c0805400f37e9169
trstndbrtl <welovehumans@gmail.com>
2019-04-21 22:39:03 +0200

    feat-MM-004 - Router prepare contact & home page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                Page contact
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                Page Home
+            </div>
+        );
+    }
+}
+
+export default Home;
