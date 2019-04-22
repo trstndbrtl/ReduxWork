f6f82c3108cabc93e5336efaf170c0cd9133e595
trstndbrtl <welovehumans@gmail.com>
2019-04-22 08:52:37 +0200

    feat-MM-006 - Tracker without middlewarre

--- src/actions/action-types.js
+++ src/actions/action-types.js
@@ -1,1 +1,2 @@
 export const SET_LOGIN = 'SET_LOGIN';
+export const SET_TRACKER = 'SET_TRACKER';
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
-import { SET_LOGIN } from './action-types';
+import { SET_LOGIN, SET_TRACKER } from './action-types';
 
 export function setLogin(isLogged){
+    return function(dispatch){
+        dispatch({
+            type : SET_LOGIN,
+            payload : isLogged
+        });
+        dispatch(setTracker());
+    }
+}
+
+export function setTracker(){
     return {
-        type : SET_LOGIN,
-        payload : isLogged
+        type : SET_TRACKER
     }
 }
